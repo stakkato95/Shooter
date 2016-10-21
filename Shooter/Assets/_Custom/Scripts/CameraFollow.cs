0e45781b6c814088909a9d3bf55fbfddf955c18b
Artsiom Kaliaha <stakkato95@gmail.com>
2016-10-21 13:07:21 +0300

    added camera tracking

---
+++ Shooter/Assets/_Custom/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5;
+
+	Vector3 offset;
+
+	void Awake () {
+		offset = transform.position - target.position;
+	}
+	
+	void FixedUpdate () {
+		var cameraTargetPosition = offset + target.position;
+		transform.position = Vector3.Lerp (transform.position, cameraTargetPosition , smoothing * Time.deltaTime);
+	}
+}
