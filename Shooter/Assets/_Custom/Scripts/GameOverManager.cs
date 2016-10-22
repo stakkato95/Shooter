82088fb9f228b75ac5b52f5697a8bae90fec1878
Artsiom Kaliaha <stakkato95@gmail.com>
2016-10-22 22:23:19 +0300

    added game over screen and enemy spawners

---
+++ Shooter/Assets/_Custom/Scripts/GameOverManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+	public PlayerHealth playerHealth;
+	public float timeBeforeLevelReloading = 4f;
+
+	Animator anim;
+	float restartTimer;
+
+	void Awake ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+
+	void Update ()
+	{
+		if (playerHealth.currentHealth <= 0) {
+			anim.SetTrigger ("GameOver");
+		}
+
+		restartTimer = Time.deltaTime;
+
+		if (restartTimer >= timeBeforeLevelReloading) {
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
